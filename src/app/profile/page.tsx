ec596bdac0aad708fd5493701e32f92bd2cfc367
NazmulHasan68 <aishadowzone@gmail.com>
2025-11-09 17:06:11 +0600

    hook

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,23 @@
 'use client'
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
 export default function Profile() {
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
+
+     console.log(searchParams.getAll('name'));
+     
+
+    const handleNavigate = () =>{
+        router.push('/')
+    }
+    
     return (
         <div>
-            profile page
+            <h1>Profile component client</h1>
+            <button onClick={handleNavigate} className="px-6 py-2 rounded-3xl bg-sky-700">Navigate to home oage</button>
         </div>
     );
 }
